4d95ef3e6c7e2a335df39c3279c3e2b7aa63f791
Karolis <karolis.martinaitis95@gmail.com>
2023-02-15 21:30:43 +0200

    Delete unused projects

--- Rybalka.Application/Dto/FishingNote/CoordinatesDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rybalka.Application.Dto.FishingNote
-{
-    public class CoordinatesDto
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-}
--- Rybalka.Domain/Interfaces/Repositories/IFishingNoteRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Rybalka.Domain.Entities;
-
-namespace Rybalka.Domain.Interfaces.Repositories
-{
-    public interface IFishingNoteRepository
-    {
-        IQueryable<FishingNote> GetFishingNotes();
-    }
-}
