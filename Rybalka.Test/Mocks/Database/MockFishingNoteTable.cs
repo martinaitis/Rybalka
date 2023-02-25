cd577e49323dc74194a256f073b86d15f0c6171c
martinaitis <karolis.martinaitis95@gmail.com>
2023-02-25 21:16:06 +0200

    Cover fishingNoteService get methods with unit tests

--- Rybalka.Test/Mocks/Database/MockFishingNoteTable.cs
+++ Rybalka.Test/Mocks/Database/MockFishingNoteTable.cs
@@ -27,6 +27,27 @@ namespace Rybalka.Test.Mocks.Database
                 CloudPct = 10,
                 ConditionText = "Sunny",
                 ImageFileName = "https://sheikasop-001-site1.atempurl.com/images/test.jpg"
+            },
+            new FishingNote
+            {
+                Id = 2,
+                User = "Paulius",
+                Title = "Pavadinimas2",
+                StartTime = new DateTime(2023, 1, 20, 10, 0, 0),
+                EndTime = new DateTime(2023, 1, 20, 12, 0, 0),
+                Latitude = 24,
+                Longitude = 52,
+                WaterBody = "Merkys",
+                FishingMethod = FishingMethod.Bottom,
+                Bait = "Sliekas",
+                FishCount = 1,
+                Description = "Daug alaus",
+                Temp = 5,
+                WindMps = 6,
+                WindDir = "S",
+                CloudPct = 80,
+                ConditionText = "Cloudy",
+                ImageFileName = "https://sheikasop-001-site1.atempurl.com/images/test2.jpg"
             }
         };
     }
--- Rybalka.Test/TestServices/FishingNoteServiceTest.cs
+++ Rybalka.Test/TestServices/FishingNoteServiceTest.cs
@@ -1,30 +1,106 @@
 ﻿using AutoMapper;
+using Moq;
 using Rybalka.Core.AutoMapper;
+using Rybalka.Core.Entities;
+using Rybalka.Core.Interfaces.Clients;
+using Rybalka.Core.Interfaces.Repositories;
 using Rybalka.Core.Services;
-using Rybalka.Test.Mocks.Clients;
-using Rybalka.Test.Mocks.Repositories;
+using Rybalka.Test.Mocks.Database;
 
 namespace Rybalka.Test.TestServices
 {
     public sealed class FishingNoteServiceTest
     {
-        private readonly Mapper _mapper = new(
-            new MapperConfiguration(
-                cfg => cfg.AddProfile(new FishingNoteProfile())));
-
         [Fact]
-        public async void WhenGettingAllFishingNotes_ThenAllFishingNotesReturn()
+        public async void GetAllFishingNotesV2_AllFishingNotesReturn()
         {
-            var fishingNoteRepositoryMock = MockIFishingNoteRepository.GetMock();
-            var weatherForecastClientMock = MockIWeatherForecastClient.GetMock();
-            var fishingNoteService = new FishingNoteService(
-                _mapper,
+            var fishingNoteRepositoryMock = new Mock<IFishingNoteRepository>();
+            fishingNoteRepositoryMock.Setup(m => m.GetFishingNotesReadOnly(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => MockFishingNoteTable.fishingNotes);
+            var fishingNoteServiceMock = new FishingNoteService(
+                GetFishingNoteMapper(),
                 fishingNoteRepositoryMock.Object,
-                weatherForecastClientMock.Object);
-
-            var result = await fishingNoteService.GetAllFishingNotes(CancellationToken.None);
+                new Mock<IWeatherForecastClient>().Object);
+            
+            var result = await fishingNoteServiceMock.GetAllFishingNotesV2(CancellationToken.None);
 
             Assert.NotNull(result);
+            Assert.Equal(MockFishingNoteTable.fishingNotes.Count, result.Count);
+        }
+
+        [Fact]
+        public async void GetFishingNoteByIdV2_WhenIdExist_ThenFishingNoteReturn()
+        {
+            var fishingNoteRepositoryMock = new Mock<IFishingNoteRepository>();
+            fishingNoteRepositoryMock
+                .Setup(m => m.GetFishingNoteByIdReadOnly(1, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => MockFishingNoteTable.fishingNotes.First());
+            var fishingNoteServiceMock = new FishingNoteService(
+               GetFishingNoteMapper(),
+               fishingNoteRepositoryMock.Object,
+               new Mock<IWeatherForecastClient>().Object);
+
+            var result = await fishingNoteServiceMock.GetFishingNoteByIdV2(1, CancellationToken.None);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async void GetFishingNoteByIdV2_WhenIdNotExist_ThenNullReturn()
+        {
+            var fishingNoteRepositoryMock = new Mock<IFishingNoteRepository>();
+            fishingNoteRepositoryMock
+                .Setup(m => m.GetFishingNoteByIdReadOnly(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => null);
+            var fishingNoteServiceMock = new FishingNoteService(
+               GetFishingNoteMapper(),
+               fishingNoteRepositoryMock.Object,
+               new Mock<IWeatherForecastClient>().Object);
+
+            var result = await fishingNoteServiceMock.GetFishingNoteByIdV2(1, CancellationToken.None);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void GetFishingNotesByUserV2_WhenUserExist_ThenFishingNotesReturn()
+        {
+            var fishingNoteRepositoryMock = new Mock<IFishingNoteRepository>();
+            fishingNoteRepositoryMock
+                .Setup(m => m.GetFishingNotesByUserReadOnly("Karolis", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => MockFishingNoteTable.fishingNotes.GetRange(0, 1));
+            var fishingNoteServiceMock = new FishingNoteService(
+               GetFishingNoteMapper(),
+               fishingNoteRepositoryMock.Object,
+               new Mock<IWeatherForecastClient>().Object);
+
+            var result = await fishingNoteServiceMock.GetFishingNotesByUserV2("Karolis", CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public async void GetFishingNotesByUserV2_WhenUserNotExist_ThenEmptyListReturn()
+        {
+            var fishingNoteRepositoryMock = new Mock<IFishingNoteRepository>();
+            fishingNoteRepositoryMock
+                .Setup(m => m.GetFishingNotesByUserReadOnly(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => new List<FishingNote>());
+            var fishingNoteServiceMock = new FishingNoteService(
+               GetFishingNoteMapper(),
+               fishingNoteRepositoryMock.Object,
+               new Mock<IWeatherForecastClient>().Object);
+
+            var result = await fishingNoteServiceMock.GetFishingNotesByUserV2("", CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        private static Mapper GetFishingNoteMapper()
+        {
+            return new(new MapperConfiguration(cfg => cfg.AddProfile(new FishingNoteProfile())));
         }
     }
 }
